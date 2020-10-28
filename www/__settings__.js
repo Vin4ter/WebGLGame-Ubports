48d6222948b729ac92982852223899ad24066e44
Vin4ter <65507989+Vin4ter@users.noreply.github.com>
2020-10-28 12:58:55 +0300

    Add files via upload

---
+++ www/__settings__.js
@@ -0,0 +1,20 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1020694.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true
+};
+SCRIPTS = [ 37196890, 37196908, 37196910, 37196894 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
